58011dda682bc17a3eb9c8c019b461c8928f9d6d
danlimax <daniellimaa05@gmail.com>
2025-12-28 10:58:10 -0300

    Correction of component responsibilities and data submission via the homepage.

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,39 +1,30 @@
 import { Button, Flex, TextInput } from "@mantine/core";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
-import { Results } from "../Results/Results";
 
-export function Search() {
-  const themeRef = useRef<HTMLInputElement>(null);
+interface Props {
+  searchTheme(theme: string): void;
+}
+export function Search({ searchTheme }: Props) {
   const [theme, setTheme] = useState("");
 
   function handleClickButton() {
-    if (!themeRef.current) {
-      return;
-    }
-    setTheme(themeRef.current.value);
+    searchTheme(theme);
   }
 
   return (
-    <Flex
-      w={{ base: 350, sm: 600, lg: 800 }}
-      bg="#343a40"
-      direction="column"
-      align="center"
-      p={16}
-      gap={16}
-    >
+    <>
       <Flex w={{ base: 300, sm: 250, lg: 400 }} align="center" gap={8}>
         <BsSearch />
         <TextInput
-          ref={themeRef}
           radius="xl"
+          value={theme}
+          onChange={(e) => setTheme(e.target.value)}
           placeholder="Digite seu tema de estudo"
           flex={1}
         />
         <Button onClick={handleClickButton}>Enviar</Button>
       </Flex>
-      {theme ? <Results theme={theme} /> : ""}
-    </Flex>
+    </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,27 @@
-import { Center } from "@mantine/core";
+import { Center, Flex } from "@mantine/core";
 import { Search } from "../components/Search/Search";
+import { useState } from "react";
+import { Results } from "../components/Results/Results";
 
 export function Home() {
+  const [searchedTheme, setSearchedTheme] = useState("");
+  function handelReciveTheme(data: string) {
+    setSearchedTheme(data);
+  }
   return (
     <>
       <Center style={{ width: "100vw", height: "100vh" }}>
-        <Search />
+        <Flex
+          w={{ base: 350, sm: 600, lg: 800 }}
+          bg="#343a40"
+          direction="column"
+          align="center"
+          p={16}
+          gap={16}
+        >
+          <Search searchTheme={handelReciveTheme} />
+          <Results theme={searchedTheme} />
+        </Flex>
       </Center>
     </>
   );
